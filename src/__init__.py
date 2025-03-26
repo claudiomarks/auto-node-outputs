f2848908835950e773dd05f0f8994c841e950123
claudiomarks <claudiomarks1@gmail.com>
2025-03-26 18:38:46 -0300

    Update 1.6.3

--- src/__init__.py
+++ src/__init__.py
@@ -13,7 +13,7 @@ from .operators.organizational_operators import (
 bl_info = {
     "name": "Auto Node Outputs",
     "author": "Claudin",
-    "version": (1, 6, 1),
+    "version": (1, 6, 3),
     "blender": (4, 3, 0),
     "location": "Compositor > Node > ViewLayer Export",
     "description": "Automatically connect ViewLayers to File Output nodes",
