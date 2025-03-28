519fb600a7714fef7a838dcc24d4a26eb34c83bc
claudiomarks <claudiomarks1@gmail.com>
2025-03-28 14:08:27 -0300

    Fixing error with export path

--- src/operators/connect_viewlayers_to_output.py
+++ src/operators/connect_viewlayers_to_output.py
@@ -195,7 +195,7 @@ class COMPOSITOR_OT_connect_viewlayers_to_output(Operator):
                 main_output_node.location = (rl_node.location.x + 400, rl_node.location.y)
                 
                 # Create file path in the new format
-                main_output_node.base_path = output_path + f"{cleaned_viewlayer_name}/{base_filename}_{cleaned_viewlayer_name}{main_bit_depth_suffix}_"
+                main_output_node.base_path = output_path + f"{cleaned_viewlayer_name}\{base_filename}_{cleaned_viewlayer_name}{main_bit_depth_suffix}_"
                 
                 # Set file format based on user selection
                 main_output_node.format.file_format = main_format
@@ -244,7 +244,7 @@ class COMPOSITOR_OT_connect_viewlayers_to_output(Operator):
                     secondary_output_node.format.exr_codec = secondary_compression
                     secondary_output_node.format.color_depth = secondary_bitdepth
                 
-                secondary_output_node.base_path = output_path + f"{cleaned_viewlayer_name}/{base_filename}_{cleaned_viewlayer_name}{secondary_bit_depth_suffix}_"
+                secondary_output_node.base_path = output_path + f"{cleaned_viewlayer_name}\{base_filename}_{cleaned_viewlayer_name}{secondary_bit_depth_suffix}_"
                 
                 # Clear existing inputs for secondary output
                 while len(secondary_output_node.inputs) > 1:
